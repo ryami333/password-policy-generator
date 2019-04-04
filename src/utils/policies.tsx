b27df1712e421d58a4a050f7b5508e5deff5fc59
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-04-04 21:47:43 +1300

    Create a couple of placeholder policies

---
+++ src/utils/policies.tsx
@@ -0,0 +1,11 @@
+export type Policy = [string, string];
+
+export type Policies = Policy[];
+
+const policies: Policy[] = [
+	["harsh truth", "harsh truths"],
+	["embarassing secret", "embarassing secrets"],
+	["ex-partner’s name", "ex-partners’ names"]
+];
+
+export default policies;
