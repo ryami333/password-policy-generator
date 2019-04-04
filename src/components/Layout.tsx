5cf56bea0dbfb60be1793c30cc67e52e1ea07569
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-04-04 20:30:25 +1300

    Create CSS variable system

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,26 +5,85 @@ import "reset.css";
 import "what-input";
 
 const Globals = createGlobalStyle`
+:root {
+  --orange: #f77754;
+  --teal: #018790;
+  --blue: #0a516d;
+  --granite: #2b2726;
+
+  --size1: calc(8 / 1680 * 100vw);
+  --size2: calc(16 / 1680 * 100vw);
+  --size3: calc(24 / 1680 * 100vw);
+  --size4: calc(32 / 1680 * 100vw);
+  --size5: calc(40 / 1680 * 100vw);
+  --size6: calc(80 / 1680 * 100vw);
+  --size7: calc(160 / 1680 * 100vw);
+
+  @media (max-width: 1024px) {
+    --size1: calc(8 / 1024 * 100vw);
+    --size2: calc(16 / 1024 * 100vw);
+    --size3: calc(24 / 1024 * 100vw);
+    --size4: calc(32 / 1024 * 100vw);
+    --size5: calc(40 / 1024 * 100vw);
+    --size6: calc(80 / 1024 * 100vw);
+    --size7: calc(160 / 1024 * 100vw);
+  }
+
+  @media (max-width: 768px) {
+    --size1: calc(8 / 768 * 100vw);
+    --size2: calc(16 / 768 * 100vw);
+    --size3: calc(24 / 768 * 100vw);
+    --size4: calc(32 / 768 * 100vw);
+    --size5: calc(40 / 768 * 100vw);
+    --size6: calc(80 / 768 * 100vw);
+    --size7: calc(160 / 768 * 100vw);
+  }
+
+  @media (max-width: 420px) {
+    --size1: calc(8 / 420 * 100vw);
+    --size2: calc(16 / 420 * 100vw);
+    --size3: calc(24 / 420 * 100vw);
+    --size4: calc(32 / 420 * 100vw);
+    --size5: calc(40 / 420 * 100vw);
+    --size6: calc(80 / 420 * 100vw);
+    --size7: calc(160 / 420 * 100vw);
+  }
+
+  --lineHeightMin: 1;
+  --lineHeightMed: 1.1;
+  --lineHeightMax: 1.3;
+
+  --color: var(--granite);
+  --lineHeight: var(--lineHeightMed);
+}
+
 html, body {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  background-color: var(--orange);
+  font-size: var(--size3);
+}
+
+body {
+  padding: var(--size2);
 }
 
 // Box sizing
 *, *:before, *:after {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 // Android applies some styles to buttons by default.
 button {
-    background: none;
-    appearance: none;
-    border: 0;
-    padding: 0;
+  background: none;
+  appearance: none;
+  border: 0;
+  padding: 0;
 }
 
 button, [role="button"] {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 
