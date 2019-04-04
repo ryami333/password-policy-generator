a1b7ad66f0fe5371a33544160c71396392930c7b
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-04-04 22:08:45 +1300

    Add some policies

--- src/utils/policies.tsx
+++ src/utils/policies.tsx
@@ -5,6 +5,9 @@ export type Policy = () => string;
 export type Policies = Policy[];
 
 const policies: Policy[] = [
+	(): string => `a sincere compliment about the shoes that I’m wearing today`,
+	(): string => `a bank statement from the last 3 calendar months`,
+	(): string => `your social-security number`,
 	(qty: number = random(1, 3)): string =>
 		`${qty} embarassing ${qty === 1 ? "secret" : "secrets"}`,
 	(qty: number = random(1, 3)): string =>
